cbc310e9f72edb64821e914397d39e32deb010d4
Valentine <valentine.nazarov@gmail.com>
2021-11-03 19:59:49 +0300

    feat: add fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,12 @@
+const path = require('path');
+const {readdir, copyFile, mkdir} = require('fs/promises');
 
+(async () => {
+  const src = path.resolve(__dirname, 'files');
+  const dest = path.resolve(__dirname, 'files-copy');
+  await mkdir(dest, {recursive: true});
+  let files = await readdir(src);
+  for (const file of files) {
+    await copyFile(path.resolve(src, file), path.resolve(dest, file));
+  }
+})();
