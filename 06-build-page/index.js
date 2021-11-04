a8d18e029d26a60b05d33d3acda4be618b8d225e
Valentine <valentine.nazarov@gmail.com>
2021-11-04 16:47:08 +0300

    refactor: get tags function

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,12 +1,25 @@
 const {createReadStream} = require('fs');
 const path = require('path');
 
-const reader = createReadStream(path.resolve(__dirname, 'template.html'), {encoding: 'utf-8'});
-const pattern = new RegExp(/{{(\w+)}}/g);
-reader.on('readable', () => {
-  let chunk = reader.read();
-  if (chunk !== null) {
-    for (const match of chunk.matchAll(pattern))
-      console.log(match[1]);
-  }
-});
+function getTagsFromHTML(html) {
+  const pattern = new RegExp(/{{(\w+)}}/g);
+  const tags = [];
+  for (const match of html.matchAll(pattern))
+    tags.push(match[1]);
+  return tags;
+}
+
+async function readHTML(root, filename) {
+  return new Promise((resolve, reject) =>{
+    const reader = createReadStream(path.resolve(root, filename), {encoding: 'utf-8'});
+    let data = '';
+    reader.on('readable', () => {
+      let chunk = reader.read();
+      if (chunk !== null) data += chunk;
+    });
+    reader.on('end', () => {resolve(data);});
+    reader.on('error', () => {reject('Error reading file');});
+  });
+}
+
+readHTML(__dirname, 'template.html').then(res => console.log(getTagsFromHTML(res)));
