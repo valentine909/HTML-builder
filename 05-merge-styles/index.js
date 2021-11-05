561e428d89db2d3d01abf582292a0ada44f080df
Valentine <valentine.nazarov@gmail.com>
2021-11-05 13:44:18 +0300

    refactor: create and export function in task 05

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -2,13 +2,24 @@ const path = require('path');
 const {readdir} = require('fs/promises');
 const {createReadStream, createWriteStream} = require('fs');
 
-const dest = path.resolve(__dirname, 'project-dist');
-const src = path.resolve(__dirname, 'styles');
-const writer = createWriteStream(path.resolve(dest, 'bundle.css'), { encoding: 'utf-8' });
+const settings = {
+  destinationFolder: 'project-dist',
+  sourceFolder: 'styles',
+  targetName: 'bundle.css',
+  extensionRegExp: new RegExp(/\.css$/g)
+};
 
-readdir(src).then(files => files.filter(file => file.match(/\.css$/g)))
-  .then(files => files.map(file => {
-    const reader = createReadStream(path.resolve(src, file), { encoding: 'utf-8' });
-    reader.pipe(writer);
-    reader.read();
-  }));
+function mergeFiles(settings){
+  const dest = path.resolve(__dirname, settings.destinationFolder);
+  const src = path.resolve(__dirname, settings.sourceFolder);
+  const writer = createWriteStream(path.resolve(dest, settings.targetName), { encoding: 'utf-8' });
+
+  readdir(src).then(files => files.filter(file => file.match(settings.extensionRegExp)))
+    .then(files => files.map(file => {
+      const reader = createReadStream(path.resolve(src, file), { encoding: 'utf-8' });
+      reader.pipe(writer);
+      reader.read();
+    }));
+}
+if(require.main === module) mergeFiles(settings);
+module.exports = mergeFiles;
