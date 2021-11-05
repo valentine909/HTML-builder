7204ceb49a988248ae0b7dde8ab368197991ea8b
Valentine <valentine.nazarov@gmail.com>
2021-11-05 15:56:53 +0300

    feat: import copydirectory and merge files functions

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,5 +1,18 @@
 const {createReadStream} = require('fs');
 const path = require('path');
+const {mergeFiles} = require('../05-merge-styles/index');
+const {copyFiles} = require('../04-copy-directory/index');
+
+const mergeSettings = {
+  destinationFolder: 'project-dist',
+  sourceFolder: 'styles',
+  targetName: 'style.css',
+  extensionRegExp: new RegExp(/\.css$/g)
+};
+const copySettings = {
+  destinationFolder: 'project-dist/assets',
+  sourceFolder: 'assets',
+};
 
 function getTagsFromHTML(html) {
   const pattern = new RegExp(/{{(\w+)}}/g);
@@ -10,16 +23,25 @@ function getTagsFromHTML(html) {
 }
 
 async function readHTML(root, filename) {
-  return new Promise((resolve, reject) =>{
+  return new Promise((resolve, reject) => {
     const reader = createReadStream(path.resolve(root, filename), {encoding: 'utf-8'});
     let data = '';
     reader.on('readable', () => {
       let chunk = reader.read();
       if (chunk !== null) data += chunk;
     });
-    reader.on('end', () => {resolve(data);});
-    reader.on('error', () => {reject('Error reading file');});
+    reader.on('end', () => {
+      resolve(data);
+    });
+    reader.on('error', () => {
+      reject('Error reading file');
+    });
   });
 }
-
-readHTML(__dirname, 'template.html').then(res => console.log(getTagsFromHTML(res)));
+async function main() {
+  await copyFiles(path.resolve(__dirname, copySettings.sourceFolder),
+    path.resolve(__dirname, copySettings.destinationFolder));
+  await mergeFiles(mergeSettings);
+  readHTML(__dirname, 'template.html').then(res => console.log(getTagsFromHTML(res)));
+}
+if(require.main === module) main().catch(err => console.log(err));
