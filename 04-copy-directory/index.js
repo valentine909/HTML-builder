8cc450a570d63256ef1aaf86769cf2148b65f441
Valentine <valentine.nazarov@gmail.com>
2021-11-05 14:23:00 +0300

    refactor: export function in task 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,12 +1,20 @@
 const path = require('path');
 const {readdir, copyFile, mkdir} = require('fs/promises');
+const folder = (require.main === module) ? __dirname : path.dirname(require.main.filename);
+const settings = {
+  destinationFolder: 'files-copy',
+  sourceFolder: 'files',
+};
 
-(async () => {
-  const src = path.resolve(__dirname, 'files');
-  const dest = path.resolve(__dirname, 'files-copy');
+async function copyFiles(settingsObj){
+  const src = path.resolve(folder, settingsObj.sourceFolder);
+  const dest = path.resolve(folder, settingsObj.destinationFolder);
   await mkdir(dest, {recursive: true});
   let files = await readdir(src);
   for (const file of files) {
     await copyFile(path.resolve(src, file), path.resolve(dest, file));
   }
-})();
+}
+
+if(require.main === module) copyFiles(settings);
+module.exports.copyFiles = copyFiles;
